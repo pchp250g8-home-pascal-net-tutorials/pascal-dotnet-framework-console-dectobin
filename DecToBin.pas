40869ab1a97e22f650f5abd7d1a31a01912e46de
Alexander <pchp250g8@tutamail.com>
2026-02-19 14:51:15 +0300

    Created Project DecToBin

---
+++ DecToBin.pas
@@ -0,0 +1,31 @@
+﻿uses
+  System;
+begin
+  var uNumber : Cardinal;
+  Console.WriteLine('Input an unsigned integer number');
+  var bIsRightNumber := (Cardinal.TryParse(Console.ReadLine(), uNumber)) and
+                     (uNumber <= Cardinal.MaxValue);
+  if (not bIsRightNumber) then
+  begin
+    Console.WriteLine('Invalid number format or number too big');
+    Console.Read();
+    exit();
+  end;
+  var strBinNum := '';
+  var uTempVal := uNumber;
+  while (uTempVal > 0) do
+  begin
+    strBinNum := uTempVal mod 2 + strBinNum;
+    uTempVal := uTempVal div 2;
+  end;
+  if (strBinNum.Length = 0) then
+  begin
+    strBinNum := '0';
+  end;
+  Console.WriteLine
+  (
+    'The binary equavalent of the decimal number {0} is: {1}',
+    uNumber, strBinNum
+  );
+  Console.Read();
+end.
