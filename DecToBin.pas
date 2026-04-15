d8c8187bf139c950af97602c21ce69b4d58a5194
Alexander <pchp250g8@tutamail.com>
2026-04-15 17:49:19 +0300

    Modified Source Code

--- DecToBin.pas
+++ DecToBin.pas
@@ -6,7 +6,7 @@ begin
   var uNumber : Cardinal;
   Console.WriteLine('Input an unsigned integer number');
   var bIsRightNumber := (Cardinal.TryParse(Console.ReadLine(), uNumber)) and
-                     (uNumber <= Cardinal.MaxValue);
+                     (uNumber <= MAX_INT);
   if (not bIsRightNumber) then
   begin
     Console.WriteLine('Invalid number format or number too big');
@@ -17,7 +17,9 @@ begin
   var uTempVal := uNumber;
   while (uTempVal > 0) do
   begin
-    strBinNum := uTempVal mod 2 + strBinNum;
+    var nBinNum := uTempVal mod 2;
+    var chBinNum := chr(nBinNum+ord('0'));
+    strBinNum := chBinNum + strBinNum;
     uTempVal := uTempVal div 2;
   end;
   if (strBinNum.Length = 0) then
